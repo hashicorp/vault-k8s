8e5ab3600f0857df3e83eb6be425f5f8d0b7c0b2
Jason O'Donnell <2160810+jasonodonnell@users.noreply.github.com>
2019-08-29 12:20:41 -0400

    Fix typo in run script

--- deployment/run.sh
+++ deployment/run.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ${DIR?}/cleanup.sh
 
-kubectl create secret generic sidecar-injector-webhook-certs \
+kubectl create secret generic sidecar-injector-certs \
    --from-file=key.pem=${DIR?}/certs/server-key.pem \
    --from-file=cert.pem=${DIR?}/certs/server-cert.pem
 
