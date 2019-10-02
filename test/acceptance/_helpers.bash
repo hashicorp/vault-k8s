460beab3f2584d87781eac5b75fec23204ef1e24
malnick <malnick@gmail.com>
2019-10-02 09:16:37 -0700

    rebase

--- test/acceptance/_helpers.bash
+++ test/acceptance/_helpers.bash
@@ -8,6 +8,39 @@ chart_dir() {
     echo ${BATS_TEST_DIRNAME}/../..
 }
 
+deploy_nginx() {
+# The nginx container will get the secrets by injection from the side car
+# which is made available by the admissions webhook. 
+#
+# The admissions webhook also needs to modify the spec to include the correct
+# service account.
+cat <<EOF | kubectl create -f -
+apiVersion: apps/v1 
+kind: Deployment
+metadata:
+  name: nginx 
+  labels:
+    app: nginx
+spec:
+  selector:
+    matchLabels:
+      app: nginx 
+  replicas: 1
+  template:
+    metadata:
+      annotations:
+        vault.hashicorp.com/agent-inject: "true"
+        vault.hashicorp.com/vault-service: "http://vault:8200"
+        vault.hashicorp.com/agent-inject-secret-foo: "kv/secret/foo:/vault/secrets/foo"
+      labels:
+        app: nginx 
+    spec:
+      containers:
+      - name: nginx 
+        image: nginx 
+EOF
+}
+
 # wait for a pod to be ready
 wait_for_running() {
     POD_NAME=$1
@@ -87,9 +120,12 @@ setup() {
   do
       kubectl exec -ti ${pod} -- vault operator unseal ${token}
   done
+
+	deploy_nginx
 }
 
 teardown() {
   helm delete --purge $(name_prefix)
+  kubectl delete deployment nginx
   kubectl delete --all pvc
 }
--- test/acceptance/acceptance.bats
+++ test/acceptance/acceptance.bats
@@ -2,15 +2,16 @@
 
 load _helpers
 
-@test "test/acceptance: testing deployment" {
-  # Sealed, not initialized
+@test "test/acceptance: testing inject" {
+	local init_status=$(kubectl exec "$(name_prefix)-0" -- vault status -format=json |
+    jq -r '.initialized')
+  [ "${init_status}" == "true" ]
+
   local sealed_status=$(kubectl exec "$(name_prefix)-0" -- vault status -format=json |
     jq -r '.sealed' )
   [ "${sealed_status}" == "false" ]
 
-  local init_status=$(kubectl exec "$(name_prefix)-0" -- vault status -format=json |
-    jq -r '.initialized')
-  [ "${init_status}" == "true" ]
+	# assuming annotations as specified in https://github.com/jasonodonnell/vault-agent-demo/blob/master/app/patch-file-annotations.yaml#L7
+  local secret_available=$(kubectl exec "$(name_prefix)-0" -- cat /vault/secrets/foo)
+  [ "${secret_available}" == "bar" ]
 }
-
-#@test "test/acceptance: testing inject" {}
