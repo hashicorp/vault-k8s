3b10543f5e39cf8bcf18a59161a45eb4e6a972f9
Khai Phan <khai.phan@tiki.vn>
2020-02-11 15:00:06 +0700

    fix container env

--- agent-inject/agent/container_env.go
+++ agent-inject/agent/container_env.go
@@ -39,7 +39,7 @@ func (a *Agent) ContainerEnvVars(init bool) ([]corev1.EnvVar, error) {
 		})
 	}
 
-	if a.ConfigMapName == "" && !a.InjectPluton {
+	if a.ConfigMapName == "" {
 		config, err := a.newConfig(init)
 		if err != nil {
 			return envs, err
