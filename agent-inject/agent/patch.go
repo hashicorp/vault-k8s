87cc8a4aad1928d5788b059131589806c220cec2
Khai Phan <khai.phan@tiki.vn>
2020-02-18 07:27:12 +0700

    update

--- agent-inject/agent/patch.go
+++ agent-inject/agent/patch.go
@@ -55,6 +55,34 @@ func addVolumeMounts(target, mounts []corev1.VolumeMount, base string) []*jsonpa
 	return result
 }
 
+func modifyEntrypoint(target, envVars []corev1.Container, base string) []*jsonpatch.JsonPatchOperation {
+	var result []*jsonpatch.JsonPatchOperation
+	first := len(target) == 0
+	var value interface{}
+	for _, v := range envVars {
+		value = v
+		path := base
+		if first {
+			first = false
+			value = []corev1.EnvVar{
+				{
+					Name:  "VAULT_ADDR",
+					Value: "addr",
+				},
+			}
+		} else {
+			path = path + "/-"
+		}
+
+		result = append(result, &jsonpatch.JsonPatchOperation{
+			Operation: "add",
+			Path:      path,
+			Value:     value,
+		})
+	}
+	return result
+}
+
 func addContainers(target, containers []corev1.Container, base string) []*jsonpatch.JsonPatchOperation {
 	var result []*jsonpatch.JsonPatchOperation
 	first := len(target) == 0
@@ -110,3 +138,32 @@ func EscapeJSONPointer(s string) string {
 	s = strings.Replace(s, "/", "~1", -1)
 	return s
 }
+
+// func mutateContainers(containers []corev1.Container) (bool, error) {
+// 	mutated := false
+// 	for i, container := range containers {
+// 		var envVars []corev1.EnvVar
+
+// 		for _, env := range container.Env {
+// 			if strings.HasPrefix(env.Value, "vault:") {
+// 				envVars = append(envVars, env)
+// 			}
+// 		}
+
+// 		if len(envVars) == 0 {
+// 			continue
+// 		}
+
+// 		mutated = true
+
+// 		// add args to command list; cmd arg arg
+// 		args := append(container.Command, container.Args...)
+
+// 		container.Command = []string{"/vault/vault-env"}
+// 		container.Args = args
+
+// 		// transform pod annotations to env vars for vault-env execution
+// 		containers[i] = container
+// 	}
+// 	return mutated, nil
+// }
--- go.mod
+++ go.mod
@@ -22,13 +22,11 @@ require (
 	github.com/pkg/errors v0.8.1
 	github.com/posener/complete v1.2.1 // indirect
 	github.com/radovskyb/watcher v1.0.7
-	github.com/stretchr/testify v1.3.0
+	github.com/stretchr/testify v1.4.0
 	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
 	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
-	k8s.io/api v0.0.0-20190409021203-6e4e0e4f393b
-	k8s.io/apimachinery v0.0.0-20190404173353-6a84e37a896d
+	k8s.io/api v0.17.3
+	k8s.io/apimachinery v0.17.3
 	k8s.io/client-go v11.0.1-0.20190409021438-1a26190bd76a+incompatible
-	k8s.io/klog v1.0.0 // indirect
 	k8s.io/utils v0.0.0-20191030222137-2b95a09bc58d // indirect
-	sigs.k8s.io/yaml v1.1.0 // indirect
 )
