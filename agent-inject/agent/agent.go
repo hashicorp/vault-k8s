8ee100735b2e5349b48c2b96b735330c2ab37c97
hc-github-team-secure-vault-ecosystem <github-team-secure-vault-ecosystem@hashicorp.com>
2021-11-17 22:45:39 +0000

    Release v0.14.1

--- agent-inject/agent/agent.go
+++ agent-inject/agent/agent.go
@@ -14,7 +14,7 @@ import (
 // TODO swap out 'github.com/mattbaird/jsonpatch' for 'github.com/evanphx/json-patch'
 
 const (
-	DefaultVaultImage                       = "hashicorp/vault:1.8.4"
+	DefaultVaultImage                       = "hashicorp/vault:1.9.0"
 	DefaultVaultAuthType                    = "kubernetes"
 	DefaultVaultAuthPath                    = "auth/kubernetes"
 	DefaultAgentRunAsUser                   = 100
