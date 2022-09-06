01c9039fc9c92f55c23a7b197803f1c446afc792
Tom Proctor <tomhjp@users.noreply.github.com>
2022-09-06 17:16:18 +0100

    Prepare to release 1.0.0

--- agent-inject/agent/agent.go
+++ agent-inject/agent/agent.go
@@ -15,7 +15,7 @@ import (
 // TODO swap out 'github.com/mattbaird/jsonpatch' for 'github.com/evanphx/json-patch'
 
 const (
-	DefaultVaultImage                       = "hashicorp/vault:1.11.1"
+	DefaultVaultImage                       = "hashicorp/vault:1.11.3"
 	DefaultVaultAuthType                    = "kubernetes"
 	DefaultVaultAuthPath                    = "auth/kubernetes"
 	DefaultAgentRunAsUser                   = 100
