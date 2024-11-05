1063509f4886033de8e8df6218a24f57f9050558
Diliz <Diliz@users.noreply.github.com>
2024-11-05 23:11:40 +0100

    Delimiter annotation (#664)
    Co-authored-by: Caitlin Elfring <celfring@gmail.com>
    Co-authored-by: Theron Voran <tvoran@users.noreply.github.com>

--- agent-inject/agent/annotations.go
+++ agent-inject/agent/annotations.go
@@ -80,6 +80,22 @@ const (
 	// If not provided, the template content key annotation is used.
 	AnnotationAgentInjectTemplateFile = "vault.hashicorp.com/agent-inject-template-file"
 
+	// AnnotationAgentInjectTemplateLeftDelim is the key annotation that configures Vault
+	// Agent what left delimiter to use for rendering the secrets.  The name
+	// of the template is any unique string after "vault.hashicorp.com/agent-template-left-delim-",
+	// such as "vault.hashicorp.com/agent-template-left-delim-foobar".  This should map
+	// to the same unique value provided in "vault.hashicorp.com/agent-inject-secret-".
+	// If not provided, a default left delimiter is used as defined by https://www.vaultproject.io/docs/agent/template#left_delimiter
+	AnnotationAgentInjectTemplateLeftDelim = "vault.hashicorp.com/agent-template-left-delim"
+
+	// AnnotationAgentInjectTemplateRightDelim is the key annotation that configures Vault
+	// Agent what right delimiter to use for rendering the secrets.  The name
+	// of the template is any unique string after "vault.hashicorp.com/agent-template-right-delim-",
+	// such as "vault.hashicorp.com/agent-template-right-delim-foobar".  This should map
+	// to the same unique value provided in "vault.hashicorp.com/agent-inject-secret-".
+	// If not provided, a default right delimiter is used as defined by https://www.vaultproject.io/docs/agent/template#right_delimiter
+	AnnotationAgentInjectTemplateRightDelim = "vault.hashicorp.com/agent-template-right-delim"
+
 	// AnnotationAgentInjectToken is the annotation key for injecting the
 	// auto-auth token into the secrets volume (e.g. /vault/secrets/token)
 	AnnotationAgentInjectToken = "vault.hashicorp.com/agent-inject-token"
@@ -646,6 +662,8 @@ func (a *Agent) secrets() ([]*Secret, error) {
 		}
 		secret.MountPath = a.annotationsSecretValue(AnnotationVaultSecretVolumePath, secret.RawName, a.Annotations[AnnotationVaultSecretVolumePath])
 		secret.Command = a.annotationsSecretValue(AnnotationAgentInjectCommand, secret.RawName, "")
+		secret.LeftDelimiter = a.annotationsSecretValue(AnnotationAgentInjectTemplateLeftDelim, secret.RawName, "")
+		secret.RightDelimiter = a.annotationsSecretValue(AnnotationAgentInjectTemplateRightDelim, secret.RawName, "")
 		secret.FilePathAndName = a.annotationsSecretValue(AnnotationAgentInjectFile, secret.RawName, "")
 		secret.FilePermission = a.annotationsSecretValue(AnnotationAgentInjectFilePermission, secret.RawName, "")
 
