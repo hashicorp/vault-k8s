1939b35490a5c989972dd41be53649fb44ca8c11
Balaji <balaji.b@hashicorp.com>
2026-04-30 14:34:43 +0530

    Release Prepare 1.7.4.

--- agent-inject/agent/agent.go
+++ agent-inject/agent/agent.go
@@ -17,7 +17,7 @@ import (
 )
 
 const (
-	DefaultVaultImage                       = "hashicorp/vault:1.21.4"
+	DefaultVaultImage                       = "hashicorp/vault:2.0.0"
 	DefaultVaultAuthType                    = "kubernetes"
 	DefaultVaultAuthPath                    = "auth/kubernetes"
 	DefaultAgentRunAsUser                   = 100
